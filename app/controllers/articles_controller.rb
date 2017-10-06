17290384193edc75ae720474634072b6952c8218
landroide13 <lazaro02@hotmail.com>
2017-10-06 15:14:53 +0700

    finish

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
 def index
   @articles = Article.all
 end
@@ -19,15 +20,12 @@ def create
 end
 
 def show
- @article = Article.find(params[:id])
 end
 
 def edit
-  @article = Article.find(params[:id])
 end
 
 def update
-  @article = Article.find(params[:id])
   if @article.update(article_params)
     flash[:success] = "Article Successfully Updated.."
     redirect_to article_path(@article)
@@ -37,7 +35,6 @@ def update
 end
 
 def destroy
-  @article = Article.find(params[:id])
   @article.destroy
   flash[:success] = "Article Successfully Destroyed.."
   redirect_to articles_path
@@ -48,6 +45,10 @@ private
 
 def article_params
   params.require(:article).permit(:title, :description)
+end
+
+def set_article
+  @article = Article.find(params[:id])
 end
 
 
