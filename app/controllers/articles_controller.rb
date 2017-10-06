ea810bcb985c26deb5c6a0c8d6ed32bc7e1436c7
landroide13 <lazaro02@hotmail.com>
2017-10-06 14:31:14 +0700

    CRUD almost

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+def index
+  @articles = Article.all
+end
+
 def new
   @article = Article.new
 end
@@ -16,6 +20,20 @@ end
 
 def show
  @article = Article.find(params[:id])
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:success] = "Article Successfully Updated.."
+    redirect_to article_path(@article)
+  else
+    render 'edit'
+  end
 end
 
 
