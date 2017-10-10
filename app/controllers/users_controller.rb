81ab99d9431473e027f8e7ac1774ba350c1d6c4a
landroide13 <lazaro02@hotmail.com>
2017-10-10 13:10:26 +0700

    Edit & Sign up users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy, :show] 
 
   def index
     @users = User.all
@@ -19,11 +20,38 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user = User.update(user_params)
+      flash[:success] = "User Updated Successfully.."
+      redirect_to articles_path
+    else
+      flash[:danger] = "Ups, something comming up, try again.."
+      render 'edit'
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @user.destroy
+    flash[:danger] = "User Successfully deleted.."
+    redirect_to articles_path
+  end
+
+
 
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
   
 
