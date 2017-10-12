7ff5b18d59f77629ad08c71794011c79ffdfcaa4
landroide13 <lazaro02@hotmail.com>
2017-10-12 11:37:43 +0700

    User's Articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "img_circle")
-  end
+  end 
 
 end
