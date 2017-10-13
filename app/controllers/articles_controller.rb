ad36bc8e79ac57a3803e127e4eb2faad67d178e2
landroide13 <lazaro02@hotmail.com>
2017-10-13 18:26:23 +0700

    Authentication/Restriction

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
 def index
   @articles = Article.paginate(page: params[:page], per_page: 2)
 end
@@ -11,7 +13,7 @@ end
 
 def create
   @article = Article.new(article_params)
-  @article.user = User.first
+  @article.user = current_user 
   if @article.save
     flash[:success] = "Article Successfully created.."
     redirect_to article_path(@article)
@@ -50,6 +52,13 @@ end
 
 def set_article
   @article = Article.find(params[:id])
+end
+
+def require_same_user
+  if current_user != @article.user
+    flash[:danger] = "You can only delete and edit your own articles.."
+    redirect_to root_path
+  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   resources :users, except: [:new]
 
-
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
