4deb1283b56215d53104474a5dc0a17dd076e739
landroide13 <lazaro02@hotmail.com>
2017-10-17 15:18:15 +0700

    Listing categories / testing

---
+++ test/integration/list_category_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @category = Category.create name:"sports"
+    @category2 = Category.create name:"arts"
+  end
+
+  test "should show categories listing" do 
+    get categories_path
+    assert_template 'categories/index'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+    #assert_match 'sports', response.body
+  end
+
+  
+end
