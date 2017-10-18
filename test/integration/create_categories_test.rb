7163f0f0b6c3d8a8a62a1cfa7973db986ed5e4e3
landroide13 <lazaro02@hotmail.com>
2017-10-18 13:01:28 +0700

    Restrictions for nonAdmins

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = User.create username:'landrade', email:'landrade13@icloud.com', password:'131313'
+  end
+
   test "get new category form and create new category" do 
+    sign_in_as(@user, '131313')
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -14,6 +19,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submision results in failure" do
+    sign_in_as(@user, '131313')
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
